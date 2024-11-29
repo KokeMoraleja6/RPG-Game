be2fb45bde9dd2e5fe509cc399c0a7fd457b125b
kdawfp <159560764+KokeMoraleja6@users.noreply.github.com>
2024-11-29 18:33:38 +0100

    Add files via upload

---
+++ app/model/Wizard.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once "Characters.php";
+
+class Wizard extends Characters{
+
+    //Properties
+    protected int $strength;   //FUERZA -> WARRIOR
+    protected int $desxterity; //DESTREZA -> HUNTER
+    protected int $constitution; //RESISTENCIA
+    protected int $intelligence;
+    protected int $wisdom;
+    protected int $charisma;
+    protected int $hp;
+    protected int $hp_now
+    protected int $exp = 0;
+    protected Armor $armor; 
+    protected Weapon $weapon;
+    protected int $x;
+    protected int $y;
+    protected array $items;
+
+    // Constructor
+    public function __construct(string $name, string $alias, string $race, int $strength, int $desxterity, int $constitution, int $intelligence, 
+    int $wisdom, int $charisma, int $hp, int $hp_now, int $x, int $y, int $exp, array $items ,bool $new_char){
+        parent::__construct(string $name, string $alias, string $race); // FALTAN LAS ARMAS Y ARMADURAS 
+        $this->name = $name;
+        $this->alias = $alias;
+        $this->race = $race;
+        if ($new_char){
+            $this->strength = 8;
+            $this->desxterity = 13;
+            $this->constitution = 14;
+            $this->intelligence = 15;
+            $this->wisdom = 12;
+            $this->charisma = 10;
+            $this->hp = 8;
+            $this->hp_now = 8;
+            $this->x = 0; // Puede cambiar según el tablero
+            $this->y = 0; // Puede cambiar según el tablero
+            $this->exp = 0;
+            $this->items = ["Little potion",];
+        } else{
+            $this->strength = $strength;
+            $this->desxterity = $desxterity;
+            $this->constitution = $constitution;
+            $this->intelligence = $intelligence;
+            $this->wisdom = $wisdom;
+            $this->charisma = $charisma;
+            $this->hp = $hp;
+            $this->hp_now = $hp_now;     
+            $this->x = $x;
+            $this->y = $y; 
+            $this->exp = $exp;
+            $this->items = $items;     
+        }   
+        // Llamamos al método guardar
+    }
+
+}
